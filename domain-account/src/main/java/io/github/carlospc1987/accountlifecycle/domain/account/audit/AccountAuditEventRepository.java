4577580f266c07a50d0f85a4f947b118d25dce30
carlosPc1987 <cadecozarrus@gmail.com>
2026-04-28 19:08:31 +0200

    Revert "Merge pull request #16 from carlosPc1987/feature/audit-foundation"
    This reverts commit 0ce3eab27196979578456b7d19000d7941ac116d, reversing
    changes made to 704723d17281b39170f5435e68710f31387a1286.

--- domain-account/src/main/java/io/github/carlospc1987/accountlifecycle/domain/account/audit/AccountAuditEventRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.carlospc1987.accountlifecycle.domain.account.audit;
-
-public interface AccountAuditEventRepository {
-
-    AccountAuditEvent save(AccountAuditEvent accountAuditEvent);
-}
